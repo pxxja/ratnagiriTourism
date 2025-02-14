655f91b4b7a27293102aef50b8f6a3638f0c2499
pxxja <poojabhambid2004@gmail.com>
2025-02-14 12:57:21 +0530

    utils file

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,14 @@
+class ApiResponse{
+    constructor(
+        statusCode,
+        message="Success",
+        data,
+    ){
+        this.statusCode=statusCode,
+        this.message=message,
+        this.data=data,
+        this.success=statusCode <499
+    }
+}
+
+export {ApiResponse}
